fd53b36f5580f1f3a6062cecc0f107e57fae91f9
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-20 23:16:57 +0300

    Controller içinden gereksiz servisler kaldırıldı. İsim değişikliği yapıldı.

---
+++ NetCoreSignalRAPI/Controllers/SampleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreSignalRAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("There is nothing here, check /positionHub");
+        }
+    }
+}

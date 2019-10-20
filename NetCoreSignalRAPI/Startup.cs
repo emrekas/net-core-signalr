f0122e9443a4887393c50ab8c362c6a493e6fce5
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-20 23:18:45 +0300

    SignalR için middleware eklendi. Hub için endpoint map edildi. Gereksiz namespaceler kaldırıldı.

--- NetCoreSignalRAPI/Startup.cs
+++ NetCoreSignalRAPI/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using NetCoreSignalRAPI.Hubs;
 
 namespace NetCoreSignalRAPI
 {
@@ -26,6 +20,7 @@ namespace NetCoreSignalRAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +40,7 @@ namespace NetCoreSignalRAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<PositionHub>("/positionHub");
             });
         }
     }

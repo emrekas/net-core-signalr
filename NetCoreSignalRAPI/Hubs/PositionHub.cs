d9027139a2fb83060eeb46b052d6287b570fdd57
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-20 23:17:27 +0300

    SignalR için hub oluşturuldu.

---
+++ NetCoreSignalRAPI/Hubs/PositionHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace NetCoreSignalRAPI.Hubs
+{
+    public class PositionHub : Hub
+    {
+        public Task ChangePosition(string user, string mouseX, string mouseY)
+        {
+            return Clients.All.SendAsync("NewPosition", user,mouseX, mouseY);
+        }
+    }
+}
